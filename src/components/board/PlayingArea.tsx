a89990690f66de9e1db1b47be5507259d9a9cd35
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-01-17 05:09:28 +0530

    updated

--- src/components/board/PlayingArea.tsx
+++ src/components/board/PlayingArea.tsx
@@ -1,5 +1,7 @@
 import Square from "../square/Square";
 
+const positions =[["R", "N", "B", "K", "Q", "B", "N", "R"],["P", "P", "P", "P", "P", "P", "P", "P"],["", "", "", "", "", "", "", ""],["", "", "", "", "", "", "", ""],["", "", "", "", "", "", "", ""],["", "", "", "", "", "", "", ""],["P", "P", "P", "P", "P", "P", "P", "P"],["R", "N", "B", "K", "Q", "B", "N", "R"]]
+
 const PlayingArea=()=>{
     return(
         <div className="w-full border aspect-square grid grid-rows-8">
@@ -9,7 +11,7 @@ const PlayingArea=()=>{
                         {
                             Array.from({length: 8}).map((_, col)=>{
                                     const index = 8* row + col
-                                    return <Square key={index} index={index}/>
+                                    return <Square object={positions[row][col]} key={index} index={index}/>
                                 }
                             )
                         }
--- src/components/square/Square.tsx
+++ src/components/square/Square.tsx
@@ -1,8 +1,8 @@
-const Square=({index}:{index: number})=>{
+const Square=({index, object}:{index: number, object: String})=>{
     const isDark = (index + Math.floor(index/8))%2==0;
     return(
         <div className="w-full aspect-square flex items-center justify-center" style={{backgroundColor:`${isDark ? "green": "whute"}`}}>
-            
+            {object}
         </div>
     )
 }
