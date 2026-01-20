86e2424f0aee3acd0471f636d33356bd81871ead
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-01-20 23:32:40 +0530

    predictive bishop paths ok

--- src/algorithms/nextmove.ts
+++ src/algorithms/nextmove.ts
@@ -15,9 +15,15 @@ export const nextPossibleMoves=(index: number, key: string)=>{
                 return [index+8]
             else
                 return [index + 8, index + 16]
+
         case "BB":
             var ret =[]
             var rank = 0
+            var p1_pruned = false
+            var p2_pruned = false
+            var p3_pruned = false
+            var p4_pruned = false
+
             while(true){
                 rank +=1
                 const p1 = index + 9*rank;
@@ -25,20 +31,34 @@ export const nextPossibleMoves=(index: number, key: string)=>{
                 const p3 = index - 7*rank;
                 const p4 = index - 9*rank;
 
-                if (p1>=0 && p1<=63){
-                    ret.push(p1);
+                if((p1-p1%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p1_pruned = true;
                 }
-                if (p2>=0 && p2<=63){
-                    ret.push(p2);
+                if((p2-p2%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p2_pruned = true;
                 }
-                if (p3>=0 && p3<=63){
-                    ret.push(p3);
+                if((p3-p3%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p3_pruned = true;
                 }
-                if (p4>=0 && p4<=63){
-                    ret.push(p4);
+                if((p4-p4%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p4_pruned = true;
                 }
+                
                 if ((p1<0 || p1>63) && (p2<0 || p2>63) && (p3<0 || p3>63) && (p4<0 || p4>63)){
                     break
+                }
+
+                if (p1>=0 && p1<=63 && !(p1_pruned)){
+                    ret.push(p1);
+                }
+                if (p2>=0 && p2<=63 && !(p2_pruned)){
+                    ret.push(p2);
+                }
+                if (p3>=0 && p3<=63 && !(p3_pruned)){
+                    ret.push(p3);
+                }
+                if (p4>=0 && p4<=63 && !(p4_pruned)){
+                    ret.push(p4);
                 }
             }
             return ret;
@@ -58,6 +78,23 @@ export const nextPossibleMoves=(index: number, key: string)=>{
                 const p3 = index - 7*rank;
                 const p4 = index - 9*rank;
 
+                if((p1-p1%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p1_pruned = true;
+                }
+                if((p2-p2%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p2_pruned = true;
+                }
+                if((p3-p3%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p3_pruned = true;
+                }
+                if((p4-p4%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p4_pruned = true;
+                }
+                
+                if ((p1<0 || p1>63) && (p2<0 || p2>63) && (p3<0 || p3>63) && (p4<0 || p4>63)){
+                    break
+                }
+
                 if (p1>=0 && p1<=63 && !(p1_pruned)){
                     ret.push(p1);
                 }
@@ -69,23 +106,6 @@ export const nextPossibleMoves=(index: number, key: string)=>{
                 }
                 if (p4>=0 && p4<=63 && !(p4_pruned)){
                     ret.push(p4);
-                }
-
-                if((p1-1)%8==0){
-                    p1_pruned = true;
-                }
-                if((p2-1)%8==0){
-                    p2_pruned = true;
-                }
-                if(p3%8==0){
-                    p3_pruned = true;
-                }
-                if(p4%8==0){
-                    p4_pruned = true;
-                }
-                
-                if ((p1<0 || p1>63) && (p2<0 || p2>63) && (p3<0 || p3>63) && (p4<0 || p4>63)){
-                    break
                 }
             }
             return ret;
