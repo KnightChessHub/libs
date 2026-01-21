7aef02fbd5f274e6995a43395ee35affe9e4fd40
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-01-21 07:00:53 +0530

    predictive queens moves completed

--- src/algorithms/nextmove.ts
+++ src/algorithms/nextmove.ts
@@ -191,6 +191,122 @@ export const nextPossibleMoves=(index: number, key: string)=>{
 
             return ret;
 
+        case "WQ":
+            var ret =[]
+            var rank = 0
+            var p1_pruned = false
+            var p2_pruned = false
+            var p3_pruned = false
+            var p4_pruned = false
+
+            while(true){
+                rank +=1
+                const p1 = index + 9*rank;
+                const p2 = index + 7*rank;
+                const p3 = index - 7*rank;
+                const p4 = index - 9*rank;
+
+                if((p1-p1%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p1_pruned = true;
+                }
+                if((p2-p2%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p2_pruned = true;
+                }
+                if((p3-p3%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p3_pruned = true;
+                }
+                if((p4-p4%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p4_pruned = true;
+                }
+                
+                if ((p1<0 || p1>63) && (p2<0 || p2>63) && (p3<0 || p3>63) && (p4<0 || p4>63)){
+                    break
+                }
+
+                if (p1>=0 && p1<=63 && !(p1_pruned)){
+                    ret.push(p1);
+                }
+                if (p2>=0 && p2<=63 && !(p2_pruned)){
+                    ret.push(p2);
+                }
+                if (p3>=0 && p3<=63 && !(p3_pruned)){
+                    ret.push(p3);
+                }
+                if (p4>=0 && p4<=63 && !(p4_pruned)){
+                    ret.push(p4);
+                }
+            }
+
+            var x = index%8;
+            var y = (index -x)/8
+
+            for(var i=0;i<8;i++){
+                if((8*i + x) != index)
+                    ret.push(8*i + x);
+                if((8*y + i) != index)
+                    ret.push(8*y + i);        
+            }
+
+            return ret;
+
+        case "BQ":
+            var ret =[]
+            var rank = 0
+            var p1_pruned = false
+            var p2_pruned = false
+            var p3_pruned = false
+            var p4_pruned = false
+
+            while(true){
+                rank +=1
+                const p1 = index + 9*rank;
+                const p2 = index + 7*rank;
+                const p3 = index - 7*rank;
+                const p4 = index - 9*rank;
+
+                if((p1-p1%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p1_pruned = true;
+                }
+                if((p2-p2%8)/8 != ((index + 8*rank)-(index + 8*rank)%8)/8){
+                    p2_pruned = true;
+                }
+                if((p3-p3%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p3_pruned = true;
+                }
+                if((p4-p4%8)/8 != ((index - 8*rank)-(index - 8*rank)%8)/8){
+                    p4_pruned = true;
+                }
+                
+                if ((p1<0 || p1>63) && (p2<0 || p2>63) && (p3<0 || p3>63) && (p4<0 || p4>63)){
+                    break
+                }
+
+                if (p1>=0 && p1<=63 && !(p1_pruned)){
+                    ret.push(p1);
+                }
+                if (p2>=0 && p2<=63 && !(p2_pruned)){
+                    ret.push(p2);
+                }
+                if (p3>=0 && p3<=63 && !(p3_pruned)){
+                    ret.push(p3);
+                }
+                if (p4>=0 && p4<=63 && !(p4_pruned)){
+                    ret.push(p4);
+                }
+            }
+
+            var x = index%8;
+            var y = (index -x)/8
+
+            for(var i=0;i<8;i++){
+                if((8*i + x) != index)
+                    ret.push(8*i + x);
+                if((8*y + i) != index)
+                    ret.push(8*y + i);        
+            }
+            
+            return ret;
+
         default:
             return []
     }
